90b9225a851fc93f8321ed776ff124d3445ad176
Tiago Carneiro Fernandes <tiagocf_13@hotmail.com>
2021-04-30 18:28:03 +0100

    no message

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
  /** Constructor method:
         this Line is a call of the superconstructor 
         has always to be first line of the constructor:**/
-        function AppClassName (id, controller) {
-            AppClassName.super_.call(this, id, controller);
+        function ZWAYTCP (id, controller) {
+            ZWAYTCP.super_.call(this, id, controller);
         }
         // Inheration call:
-        inherits(AppClassName, AutomationModule);
+        inherits(ZWAYTCP, AutomationModule);
         //definition of the class reference:
-        _module = AppClassName;
+        _module = ZWAYTCP;
         /** Initialization method:
         Variable for referenciate to the class in own methods, 
         because this is context dependent in JavaScript
         Here you can register listeners see EventBus **/
-        AppClassName.prototype.init = function (config) {
-            AppClassName.super_.prototype.init.call(this, config);
+        ZWAYTCP.prototype.init = function (config) {
+            ZWAYTCP.super_.prototype.init.call(this, config);
             var self = this;
-			self.controller.devices.on({deviceId}, {event}, function() { 
-				self.controller.addNotification("critical", deviceId + "" + event, "tcp", "TCP Handler");
+			self.controller.devices.on('change:metrics', function(device) { 
+				self.controller.addNotification("critical", deviceId + "" + device.toJSON(), "tcp", "TCP Handler");
+				console.log(device.toJSON());
 			 });
         };
 
@@ -30,14 +31,14 @@
 
         /** Destroy method:
         Here you have to unregister Listeners see EventBus **/
-        AppClassName.prototype.stop = function () {
-            AppClassName.super_.prototype.stop.call(this);
-			this.controller.devices.off({deviceId}, {event}, function() {});
+        ZWAYTCP.prototype.stop = function () {
+            ZWAYTCP.super_.prototype.stop.call(this);
+			//this.controller.devices.off('change:metrics', function() {});
         };
         //Own methods: Write your own methods here
-        AppClassName.prototype.handler = function(command, args){
-			console.log("command: " + command);
-			console.log("args: " + args);
-        }
+        //AppClassName.prototype.handler = function(command, args){
+		//	console.log("command: " + command);
+		//	console.log("args: " + args);
+        //}
 
 		

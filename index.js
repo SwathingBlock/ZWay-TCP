5db5d75eb9f7a39c9fa38910997a082671fc0f2f
Tiago Carneiro Fernandes <tiagocf_13@hotmail.com>
2021-05-04 14:30:27 +0100

    no message

--- index.js
+++ index.js
@@ -1,44 +1,98 @@
- /** Constructor method:
-        this Line is a call of the superconstructor 
-        has always to be first line of the constructor:**/
+ 
+/**
+ * @author Tiago Fernandes
+ * @since 03/05/2021
+ */
+
+/**
+ * 
+ * @param {*} id 
+ * @param {*} controller 
+ */
         function ZWAYTCP (id, controller) {
+            console.log("Initializing ZWAYTCP module");
             ZWAYTCP.super_.call(this, id, controller);
         }
-        // Inheration call:
+
+
+
         inherits(ZWAYTCP, AutomationModule);
-        //definition of the class reference:
         _module = ZWAYTCP;
-        /** Initialization method:
-        Variable for referenciate to the class in own methods, 
-        because this is context dependent in JavaScript
-        Here you can register listeners see EventBus **/
+
+
+
+
+        //definition of the class reference:
+        // Initialization method:
         ZWAYTCP.prototype.init = function (config) {
             ZWAYTCP.super_.prototype.init.call(this, config);
             var self = this;
-			self.controller.devices.on('change:metrics', function(device) { 
+
+            self.callback = _.bind(self.updateDevice, self);
+            self.controller.devices.on("change:metrics:level", self.callback);
+            
+			/*self.controller.devices.on('change:metrics', function(device) { 
 				self.controller.addNotification("critical", deviceId + "" + device.toJSON(), "tcp", "TCP Handler");
 				console.log(device.toJSON());
-			 });
+			 });*/
         };
-
-
-
-
-
-
-
-
 
         /** Destroy method:
         Here you have to unregister Listeners see EventBus **/
         ZWAYTCP.prototype.stop = function () {
+            var self = this;
+            self.controller.devices.off("change:metrics:level", self.callback)
             ZWAYTCP.super_.prototype.stop.call(this);
-			//this.controller.devices.off('change:metrics', function() {});
+            console.log("ZWAYTP stopped");
         };
-        //Own methods: Write your own methods here
-        //AppClassName.prototype.handler = function(command, args){
-		//	console.log("command: " + command);
-		//	console.log("args: " + args);
-        //}
+
+
+
+        ZWAYTCP.prototype.updateDevice = function (device) {
+            var self = this;
+            var value = device.get("metrics:level");
+            
+            // if (device.get("deviceType") == "switchBinary" || device.get("deviceType") == "sensorBinary") {
+                // if (value == 0) {
+                //     value = "off";
+                // } else if (value == 255) {
+                //     value = "on";
+                // }
+            // }
+        
+            self.get_url(device, value);
+        };
+
+
+
+        HTZWAYTCPTPGet.prototype.get_url = function (device, value) {
+
+            var url = this.config.url // get url property, defined on module.json, 
+            url = url.replace("%DEVICE%",device.id);
+            url = url.replace("%VALUE%",value);
+        
+            console.log(this.config.url+" : "+device.id+" : "+value+" : "+url);
+        
+            var req = {
+                url: url,
+                async: true,
+                success: function(response) {
+                    console.log("Request was successful");
+                    },
+                error: function(response) {
+                    console.log("Can not make request: " + response.statusText); // don't add it to notifications, since it will fill all the notifcations on error
+                    } 
+                };
+        
+        // With authorization - untested
+        //        if (self.config.user !== undefined && self.config.password !== undefined) {
+        //            req.auth = {
+        //                    login: self.config.login,
+        //                    password: self.config.password
+        //            };
+        //        }
+
+            http.request(req);
+        };
 
 		
